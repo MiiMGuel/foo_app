cccbae5b4bcbde0f567e052fc5c87d6fb10fb068
MiiMGuel <miiguel065+github.com@gmail.com>
2025-05-24 14:14:23 +0700

    shyt works

--- src/foo.c
+++ src/foo.c
@@ -1,33 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <memory.h>
 
-#include "main.h"
-#include "types.h"
+#include "main.c"
 
 typedef struct foo_data {
-    i32 argc;
+    int32_t argc;
     char** argv;
 } foo_data_t;
 
-static void _foo_start(void* app_data) {
+static void _foo_start(uintptr_t* app_data) {
     foo_data_t* foo = (foo_data_t*)app_data;
 
     printf("Hello, World!\n");
     printf("args: %d\n", foo->argc);
 
-    for (i32 i = 0; i < foo->argc; i++) {
+    for (int32_t i = 0; i < foo->argc; i++) {
         printf("\t- %s\n", foo->argv[i]);
     }
 }
 
 // close() code below start() code for easier 
 // object management (construction and deconstruction)
-static void _foo_close(void* app_data) {
+static void _foo_close(uintptr_t* app_data) {
     foo_data_t* foo = (foo_data_t*)app_data;
 }
 
-static void _foo_run(void* app_data) {
+static void _foo_run(uintptr_t* app_data) {
     foo_data_t* foo = (foo_data_t*)app_data;
 }
 
@@ -39,7 +39,7 @@ app_info_t main_app(int argc, char* argv[]) {
     data->argv = argv;
 
     return (app_info_t){
-        .app_data = (void*)data,
+        .app_data = (uintptr_t*)data,
         .start    = _foo_start,
         .run      = _foo_run,
         .close    = _foo_close
--- src/main.c
+++ src/main.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
-#include "main.h"
+typedef struct app_info {
+    void (*start)(uintptr_t* app_data);
+    void (*run)(uintptr_t* app_data);
+    void (*close)(uintptr_t* app_data);
+    // will be freed after _main_app.close() is called
+    uintptr_t* app_data;
+} app_info_t;
+
+app_info_t main_app(int argc, char* argv[]);
 
 static app_info_t _main_app = { 0 };
 
